90c80dca61d59ef7b54a3949e52b6539eaa10b84
13711039880 <fengD54@163.com>
2025-05-25 09:15:42 +0800

    改了一点东西

--- src/test/java/org/a8043/test/Main.java
+++ src/test/java/org/a8043/test/Main.java
@@ -1,5 +1,6 @@
+package org.a8043.test;
+
 import org.a8043.simpleFramework.SimpleFrameworkApplication;
-import org.a8043.test.App;
 
 public class Main {
     public static void main(String[] args) {
